3f4d7cb0c924f8fd6c6372d49b6878503a461a4a
KripaSharma2016 <kripashankarsharma95@gmail.com>
2025-06-28 21:08:10 +0530

    Sweager router added

--- library-app/src/routes/router.go
+++ library-app/src/routes/router.go
@@ -1,19 +1,24 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "library-app/src/controllers"
+	_ "library-app/docs"
+	"library-app/src/controllers"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetupRouter() *gin.Engine {
-    r := gin.Default()
-    api := r.Group("/books")
-    {
-        api.GET("", controllers.GetBooks)
-        api.GET("/:id", controllers.GetBook)
-        api.POST("", controllers.CreateBook)
-        api.PUT("/:id", controllers.UpdateBook)
-        api.DELETE("/:id", controllers.DeleteBook)
-    }
-    return r
+	r := gin.Default()
+	api := r.Group("/books")
+	{
+		api.GET("", controllers.GetBooks)
+		api.GET("/:id", controllers.GetBook)
+		api.POST("", controllers.CreateBook)
+		api.PUT("/:id", controllers.UpdateBook)
+		api.DELETE("/:id", controllers.DeleteBook)
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	return r
 }
